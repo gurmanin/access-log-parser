6bf8eea6cb55d771516db255e3313d517f5d7c3c
gurmanin <artemyev178@gmail.com>
2025-11-21 15:31:57 +0300

    Revert "задание курсового проекта "Синтаксис языка"

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
         int a = new Scanner(System.in).nextInt();
         System.out.println("Введите еще одно число и нажмите <Enter>");
         int b = new Scanner(System.in).nextInt();
-        double c =(double)a/(double)b;
-        System.out.println("Сумма введенных чисел равна:" + (a+b));
-        System.out.println("Разность введенных чисел равна:" + (a-b));
-        System.out.println("Произведение введенных чисел равно:" + (a*b));
-        System.out.println("Частное введенных чисел равно:" + c);
+        double c = (double) a / (double) b;
+        System.out.println("Сумма введенных чисел равна:" + (a + b));
+        System.out.println("Разность введенных чисел равна:" + (a - b));
+        System.out.println("Произведение введенных чисел равно:" + (a * b));
+        if (c % 1 != 0) {
+            System.out.println("Частное введенных чисел равно:" + c);
+        } else {
+            System.out.println("Частное введенных чисел равно:" + (int) c);
+        }
+
     }
 }
