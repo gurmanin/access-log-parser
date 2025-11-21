27fc2ba28badf7288fcfa701ddc6543174eb2479
gurmanin <artemyev178@gmail.com>
2025-11-21 15:04:49 +0300

    выполнение задания курсового проекта "Синтаксис языка" и "Базовые типы даных"

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число и нажмите <Enter>");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Введите еще одно число и нажмите <Enter>");
+        int b = new Scanner(System.in).nextInt();
+        double c =(double)a/(double)b;
+        System.out.println("Сумма введенных чисел равна:" + (a+b));
+        System.out.println("Разность введенных чисел равна:" + (a-b));
+        System.out.println("Произведение введенных чисел равно:" + (a*b));
+        System.out.println("Частное введенных чисел равно:" + c);
     }
 }
