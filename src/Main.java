6f2cec47fed611c6c93a6f702d8115613c9db198
gurmanin <artemyev178@gmail.com>
2025-12-03 17:28:21 +0300

    Задание второго курса

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Введите текст и нажмите <Enter>");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
